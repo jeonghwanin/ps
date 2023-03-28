42aabb47e9a9575a040f7112d843589b7e3074b8
Jeonghwan In <jhpc0128@naver.com>
2023-03-29 06:58:33 +0900

    230329

--- SWEA/Pro 연습 1.오타교정시스템/Pro 연습 1.오타교정시스템/소스.cpp
+++ SWEA/Pro 연습 1.오타교정시스템/Pro 연습 1.오타교정시스템/소스.cpp
@@ -13,66 +13,12 @@ unordered_map<string, string>DB[5]; //오타 교정 DB
 unordered_map<string, int>DB_CNT;// 오타 교정 DB 개수
 Person p[2001];
 
-// The below commented functions are for your reference. If you want 
-// to use it, uncomment these functions.
-/*
-int mstrcmp(const char *a, const char *b)
-{
-	int i;
-	for (i = 0; a[i] != '\0'; i++)
-	{
-		if (a[i] != b[i])
-			return a[i] - b[i];
-	}
-	return a[i] - b[i];
-}
-
-int mstrncmp(const char *a, const char *b, int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		if (a[i] != b[i])
-			return a[i] - b[i];
-	}
-	return 0;
-}
-
-int mstrlen(const char *a)
-{
-	int len = 0;
-
-	while (a[len] != '\0')
-		len++;
-
-	return len;
-}
-
-void mstrcpy(char *dest, const char *src)
-{
-	int i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = src[i];
-}
-
-void mstrncpy(char *dest, const char *src, int len)
-{
-	for (int i = 0; i<len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[len] = '\0';
-}
-*/
 
 void init(int n) {
 	LOG.clear();
-	for(int t=0; t<5; ++t)DB[t].clear();
+	for(int t=0; t<5; ++t) DB[t].clear();
 	DB_CNT.clear();
-	for (int t = 1; t <= 2000; ++t) p[t].last = -11;
+	for (int t = 1; t <= n; ++t) p[t].last = -11;
 }
 
 int search(int mId, int searchTimestamp, char searchWord[11], char correctWord[5][11]) {
@@ -127,7 +73,7 @@ int search(int mId, int searchTimestamp, char searchWord[11], char correctWord[5
 			}
 		}
 		//오타 후보면 등록
-		string str = p[mId].str + s;
+		string str = p[mId].str + '+' + s;
 
 		if (find) {
 			if (LOG[str] < 3) {
@@ -154,6 +100,8 @@ int search(int mId, int searchTimestamp, char searchWord[11], char correctWord[5
 			}
 			correctWord[t][q] = '\0';
 		}
+		p[mId].str = s;
+		p[mId].last = searchTimestamp;
 		return n;
 	}
 }
